e80eb5df1c39cc6546c904a1e6537d65cab32895
Silviu D. Bojica <silviu@cubicleapps.com>
2014-09-20 08:55:53 +0000

    Number conversion

--- javascript_pro_web/examples_ch03/examples.js
+++ javascript_pro_web/examples_ch03/examples.js
@@ -2,6 +2,7 @@ if (Meteor.isClient) {
   // direct code here, uncomment code snippet to see how is working:
 
   // NaN (Not a Number)
+  // ==================
   // alert(NaN == NaN);    // false, recommended isNaN(NaN) for correct answer
   // alert(isNaN(NaN));    // true
   // alert(isNaN(10));     // false - 10 is a number
@@ -11,6 +12,9 @@ if (Meteor.isClient) {
   // alert(isNaN(false));  // false - can be converted to number 0
 
   // Number Conversions
+  // ==================
+  // Number
+  //-------
   // var num1 = Number("Hello world!");
   // console.log(num1); // NaN
   // var num2 = Number("");
@@ -21,4 +25,56 @@ if (Meteor.isClient) {
   // console.log(num4); // 1
   // var num5 = Number(false);
   // console.log(num5); // 0
+  // var num6 = Number(22.5);
+  // console.log(num6); // 22.5
+  // parseInt
+  // --------
+  // var num1 = parseInt("1234blue");
+  // console.log(num1); // 1234
+  // var num2 = parseInt("");
+  // console.log(num2); // NaN
+  // var num3 = parseInt("0xA");
+  // console.log(num3); // 10 - hexadecimal
+  // var num4 = parseInt(22.5);
+  // console.log(num4); // 22
+  // var num5 = parseInt("70");
+  // console.log(num5); // 70 - decimal
+  // var num6 = parseInt("0xf");
+  // console.log(num6); // 15 - hexadecimal
+  // var num7 = parseInt(011);
+  // console.log(num7); // 9 - octal !!!
+  // var num8 = parseInt("011");
+  // console.log(num8); // 11 - decimal !!!
+  // parseInt with radix as second parameter
+  // ---------------------------------------
+  // var num1 = parseInt("0xAF", 16);
+  // console.log(num1); // 175
+  // var num2 = parseInt("AF", 16);
+  // console.log(num2); // 175
+  // var num3 = parseInt("AF");
+  // console.log(num3); // NaN
+  // var num4 = parseInt("10", 2);
+  // console.log(num4); // 2 - parsed as binary
+  // var num5 = parseInt("10", 8);
+  // console.log(num5); // 8 - parsed as octal
+  // var num6 = parseInt("10", 10);
+  // console.log(num6); // 10 - parsed as decimal
+  // var num7 = parseInt("10", 16);
+  // console.log(num7); // 16 - parsed as hexadecimal
+  // parseFloat
+  // ----------
+ // var num1 = parseFloat("1234blue");
+ // console.log(num1); // 1234 - integer
+ // var num1a = parseFloat(true);
+ // console.log(num1a); // NaN
+ // var num2 = parseFloat("0xA");
+ // console.log(num2); // 0
+ // var num3 = parseFloat("22.5");
+ // console.log(num3); // 22.5
+ // var num4 = parseFloat("22.34.5");
+ // console.log(num4); // 22.34
+ // var num5 = parseFloat("0908.5");
+ // console.log(num5); // 908.5
+ // var num6 = parseFloat("3.125e7");
+ // console.log(num6); // 31250000
 }
