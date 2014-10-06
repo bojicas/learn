1758a33562d2595ed74bc01acbd17eaf19e6fe75
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-06 13:55:15 +0400

    Numbers conversion

---
+++ javascript_pro/chapter_03/02_numbers.js
@@ -0,0 +1,19 @@
+// parseInt conversion
+
+var num1 = parseInt("1234blue");
+console.log(num1);
+
+var num2 = parseInt("");
+console.log(num2);
+
+var num3 = parseInt("0xA");
+console.log(num3);
+
+var num4 = parseInt(22.5);
+console.log(num4);
+
+var num5 = parseInt("70");
+console.log(num5);
+
+var num6 = parseInt(0xf);
+console.log(num6);
