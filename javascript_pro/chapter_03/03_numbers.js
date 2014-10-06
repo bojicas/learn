e272fe32cf0a29f5d7b552ba60f58d197afab5d2
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-06 13:59:14 +0400

    parseInt with radix

---
+++ javascript_pro/chapter_03/03_numbers.js
@@ -0,0 +1,13 @@
+// parseInt with radix
+
+var num1 = parseInt("10", 2);
+console.log(num1);
+
+var num2 = parseInt("10", 8);
+console.log(num2);
+
+var num3 = parseInt("10", 10);
+console.log(num3);
+
+var num4 = parseInt("10", 16);
+console.log(num4);
