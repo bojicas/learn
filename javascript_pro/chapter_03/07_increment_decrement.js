51c1dfcaa47167d9cb3d69711c7ea32a7cd38aca
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-06 14:47:56 +0400

    Unary operators: Increment & Decrement

---
+++ javascript_pro/chapter_03/07_increment_decrement.js
@@ -0,0 +1,33 @@
+// Unary Operators
+// Increment/Decrement
+
+var age = 29;
+++age;
+console.log(age);
+
+var age1 = 29;
+--age1;
+console.log(age1);
+
+var age2 = 29;
+var anotherAge = --age2 + 2;
+console.log(age2);
+console.log(anotherAge);
+
+var num1 = 2;
+var num2 = 20;
+var num3 = --num1 + num2;
+var num4 = num1 + num2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+var num01 = 2;
+var num02 = 20;
+var num03 = num01-- + num02;
+var num04 = num01 + num02;
+console.log(num01);
+console.log(num02);
+console.log(num03);
+console.log(num04);
