01e91b50d1c7e56d252b69fbb9325d3dcb12eef7
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-06 23:36:31 +0400

    Unary plus operator

---
+++ javascript_pro/chapter_03/09_unary_plus_minus.js
@@ -0,0 +1,30 @@
+// Unary Plus and Minus
+
+var num = 25;
+num = +num;
+console.log(num);
+
+var s1 = "01";
+var s2 = "1.1";
+var s3 = "z";
+var b = false;
+var f = 1.1;
+var o = {
+  valueOf: function () {
+    return -1;
+  }
+};
+
+s1 = +s1;
+s2 = +s2;
+s3 = +s3;
+b = +b;
+f = +f;
+o = +o;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(b);
+console.log(f);
+console.log(o);
