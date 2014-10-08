047a281cf4fb62469877d19815376765778f8e3d
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-08 23:01:13 +0400

    Identically Equal and Not Identically Equal

---
+++ javascript_pro/chapter_03/20_identically_equal.js
@@ -0,0 +1,13 @@
+// Identically Equal and Not Identically Equal
+
+var result1 = ("55" == 55);
+var result2 = ("55" === 55);
+
+console.log(result1);
+console.log(result2);
+
+result1 = ("55" != 55);
+result2 = ("55" !== 55);
+
+console.log(result1);
+console.log(result2);
