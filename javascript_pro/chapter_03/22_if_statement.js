451c5ff9ef980d2f19cdb7424ca9a0fae23174d1
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-08 23:31:58 +0400

    If Statement

---
+++ javascript_pro/chapter_03/22_if_statement.js
@@ -0,0 +1,19 @@
+// The if Statement
+
+var i = 30;
+
+if (i > 25) {
+  console.log("Greater than 25.");
+} else {
+  console.log("Less than or equal to 25.");
+}
+
+i = 25;
+
+if (i > 25) {
+  console.log("Greater than 25;");
+} else if (i < 0) {
+  console.log("Less than 0.");
+} else {
+  console.log("Between 0 and 25, inclusive.");
+}
