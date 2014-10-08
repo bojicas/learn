9157f8ae2a1ffc1ecd899e5d930ec1bd2dc4a56e
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-08 22:00:53 +0400

    Equality Operator - Equal and Not Equal

---
+++ javascript_pro/chapter_03/equal_and_not_equal.js
@@ -0,0 +1,15 @@
+// Equality Operators
+
+// Equal and Not Equal
+
+console.log(null == undefined); // true
+console.log("NaN" == NaN); // false
+console.log(5 == NaN); //false
+console.log(NaN == NaN); // false
+console.log(NaN != NaN); // true
+console.log(false == 0); // true
+console.log(true == 1); // true
+console.log(true == 2); // false
+console.log(undefined == 0); // false
+console.log(null == 0); // false
+console.log("5" == 5);
