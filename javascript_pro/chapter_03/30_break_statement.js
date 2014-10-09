bc2ad6ceb38c1662f808a2fc8563f4a811806631
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-09 18:55:24 +0400

    The break statement and label statements

---
+++ javascript_pro/chapter_03/30_break_statement.js
@@ -0,0 +1,15 @@
+// The break statement and label statements
+
+var num = 0;
+
+outermost: for (var i = 0; i < 10; i++) {
+  for (var j = 0; j < 10; j++) {
+    if (i === 5 && j === 5) {
+      break outermost;
+    }
+    num++;
+  }
+}
+
+console.log(num);
+
