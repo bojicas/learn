6de8eda4af0ce7eacf939721b3f0e45d3eb17c51
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-10 02:02:04 +0400

    Switch Statement (unique characteristics)

---
+++ javascript_pro/chapter_03/35_switch_statement.js
@@ -0,0 +1,12 @@
+// Switch Statement (unique characteristics)
+
+switch ("hello world") {
+  case "hello" + " world":
+    console.log("Greeting was found.");
+    break;
+  case "goodbye":
+    console.log("Closing was found.");
+    break;
+  default:
+    console.log("Unexpected message was found.");
+}
