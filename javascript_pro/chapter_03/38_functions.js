edc1f99d89f24f7da1d0fff8beb1df68f1dcdf98
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-10 10:35:26 +0400

    Functions - returning a value

---
+++ javascript_pro/chapter_03/38_functions.js
@@ -0,0 +1,10 @@
+// Functions - returning a value
+
+function sum(num1, num2) {
+  return num1 + num2;
+  console.log("This is never executed");
+}
+
+var result = sum(5, 10);
+
+console.log(result);
