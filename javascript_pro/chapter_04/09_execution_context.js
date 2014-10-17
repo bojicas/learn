bd16f75622e28dddbea38a2eab950b11733b5fe8
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-17 18:53:53 +0000

    Execution context

---
+++ javascript_pro/chapter_04/09_execution_context.js
@@ -0,0 +1,17 @@
+var color = "blue";
+
+function changeColor() {
+  var anotherColor = "red";
+
+  function swapColors() {
+    var tempColor = anotherColor;
+    anotherColor = color;
+    color = tempColor
+  }
+
+  swapColors();
+}
+
+changeColor();
+
+console.log(color);
