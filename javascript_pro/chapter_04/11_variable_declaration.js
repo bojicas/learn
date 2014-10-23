1ed61fad6a1860eb945ab7e17d7bfdabb46d0ad9
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-24 00:02:55 +0400

    Variable declaration

---
+++ javascript_pro/chapter_04/11_variable_declaration.js
@@ -0,0 +1,10 @@
+// Variable Declaration
+
+function add(num1, num2) {
+  var sum = num1 + num2;
+  return sum;
+}
+
+var result = add(10, 20);
+// console.log(sum); // ReferenceError: sum is not defined
+console.log(result);
