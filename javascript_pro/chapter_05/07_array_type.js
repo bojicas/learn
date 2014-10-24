6949ca5868814cbadd13bbbdedbeac4fa45d6a55
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-25 00:26:20 +0400

    Array Type - literal notation

---
+++ javascript_pro/chapter_05/07_array_type.js
@@ -0,0 +1,11 @@
+// Array Type - literal notation
+
+var colors = ["red", "blue", "green"];
+var names = [];
+var values = [1, 2, ]; // avoid
+var options = [,,,,,]; // avoid
+
+console.log(colors);
+console.log(names);
+console.log(values);
+console.log(options);
