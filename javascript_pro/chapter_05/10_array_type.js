2b5ed2d50c98e741aa07890fc166edd7bcf4db85
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-25 00:49:48 +0400

    Array Type - add to array using length property

---
+++ javascript_pro/chapter_05/10_array_type.js
@@ -0,0 +1,7 @@
+// Array Type - add to array using length property
+
+var colors = ["red", "green", "blue"];
+colors[colors.length] = "black";
+colors[colors.length] = "brown";
+
+console.log(colors);
