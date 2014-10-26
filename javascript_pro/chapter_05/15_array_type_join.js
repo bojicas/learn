b3e87f1475192c983ab1097d1dbac4cf360b2d91
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-27 00:26:45 +0400

    Array Type - join() method

---
+++ javascript_pro/chapter_05/15_array_type_join.js
@@ -0,0 +1,6 @@
+// Array Type - join() method
+
+var colors = ["red", "green", "blue"];
+console.log(colors.join(","));  // red,green,blue
+console.log(colors.join());     // red,green,blue
+console.log(colors.join("||")); // red||green||blue
