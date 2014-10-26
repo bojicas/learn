dc1126deb4126c2c509f676f29a9e4a00ebb39e1
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-27 00:10:49 +0400

    Array Type - conversion methods

---
+++ javascript_pro/chapter_05/13_conversion_methods.js
@@ -0,0 +1,7 @@
+// Array Type - conversion methods
+
+var colors = ["red", "blue", "green"];
+
+console.log(colors.toString()); // red,blue,green
+console.log(colors.valueOf());  // [ 'red', 'blue', 'green' ]
+console.log(colors);            // [ 'red', 'blue', 'green' ]
