9572e19d6bb0a12691da88958c13554f960d8857
Silviu D. Bojica <silviu@cubicleapps.com>
2014-10-28 22:33:07 +0400

    Array Type - iterative methods forEach()

---
+++ javascript_pro/chapter_05/31_array_foreach.js
@@ -0,0 +1,7 @@
+// Array Type - iterative methods forEach()
+
+var numbers = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+
+numbers.forEach(function (item, index, array) {
+  console.log(item + ' at index ' + index + ' in array ' + array);
+});
