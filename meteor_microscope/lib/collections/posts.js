eefe5a38ab0b7057f74363cb23f3ed649c52dbb0
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-02 11:07:24 +0400

    7-7 Enforce post URL uniqueness

--- meteor_microscope/lib/collections/posts.js
+++ meteor_microscope/lib/collections/posts.js
@@ -7,13 +7,24 @@ Meteor.methods({
       title: String,
       url: String
     });
+
+    var postWithSameLink = Posts.findOne({ url: postAttributes.url });
+    if (postWithSameLink) {
+      return {
+        postExists: true,
+        _id: postWithSameLink._id
+      }
+    }
+
     var user = Meteor.user();
     var post = _.extend(postAttributes, {
       userId: user._id,
       author: user.username,
       submitted: new Date()
     });
+
     var postId = Posts.insert(post);
+
     return {
       _id: postId
     };
