9c100d4fbf4d00f8e6279b17e017ab5ee0d99b05
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-05 00:50:12 +0400

    10-2 Display comments on postPage

--- meteor_microscope/client/templates/posts/post_item.js
+++ meteor_microscope/client/templates/posts/post_item.js
@@ -2,9 +2,14 @@ Template.postItem.helpers({
   ownPost: function () {
     return this.userId === Meteor.userId();
   },
+
   domain: function () {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+
+  commentsCount: function () {
+    return Comments.find({ postId: this._id }).count();
   }
 });
