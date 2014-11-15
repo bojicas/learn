ec1faf3a5b5f3b594fc4f1e9b7390cdf0c9668b6
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-15 15:11:19 +0400

    Functional Internals (factorial example - recursive)

---
+++ javascript_pro/chapter_05/57_function_internals.js
@@ -0,0 +1,11 @@
+// Function Internals (factorial example)
+
+function factorial(num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(5));
