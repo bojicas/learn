caebb41977869c458437f1e8a80a70c81147e429
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-15 15:16:42 +0400

    Decoupling recursive factorial call

---
+++ javascript_pro/chapter_05/58_function_type_args.js
@@ -0,0 +1,20 @@
+// Function Internals (arguments.callee)
+
+function factorial(num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * arguments.callee(num - 1);
+  }
+}
+
+console.log(factorial(5)); // 120
+
+var trueFactorial = factorial;
+
+factorial = function () {
+  return 0;
+};
+
+console.log(trueFactorial(5)); // 120
+console.log(factorial(5));     // 0
