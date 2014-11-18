bcc909dd7f3620a672954382358b427a67b425b6
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-18 08:27:35 +0400

    Function Type - call() method

---
+++ javascript_pro/chapter_05/64_function_type_call.js
@@ -0,0 +1,11 @@
+// Function Type - call() method
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function callSum(num1, num2) {
+  return sum.call(this, num1, num2);
+}
+
+console.log(callSum(10, 10));
