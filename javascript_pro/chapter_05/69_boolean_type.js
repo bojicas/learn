91b593769b14ed91589641cb419f477def0a87ca
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-20 20:22:48 +0400

    Boolean Type

---
+++ javascript_pro/chapter_05/69_boolean_type.js
@@ -0,0 +1,9 @@
+// Boolean Type
+
+var falseObject = new Boolean(false);
+var result = falseObject && true;
+console.log(result); // true
+
+var falseValue = false;
+result = falseValue && true;
+console.log(result); // false
