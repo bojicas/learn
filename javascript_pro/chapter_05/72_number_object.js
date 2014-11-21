33324a6c8b4a5d8ac4b9986e4187f905655be3ef
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-21 22:38:20 +0400

    Number Type Example

---
+++ javascript_pro/chapter_05/72_number_object.js
@@ -0,0 +1,10 @@
+// Number Object
+
+var numberObject = new Number(10);
+var numberValue = 10;
+
+console.log(typeof numberObject); // object
+console.log(typeof numberValue);  // number
+
+console.log(numberObject instanceof Number);  // true
+console.log(numberValue instanceof Number);   // false
