3945860be77641fb7aaae440c64c6bc8b1240e1a
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-22 11:08:22 +0400

    String Pattern Matching Methods - how to htmlEscape

--- javascript_pro/chapter_05/75_string_pattern_matching.js
+++ javascript_pro/chapter_05/75_string_pattern_matching.js
@@ -26,7 +26,26 @@ console.log(text);
 result = text.replace(/at/g, 'ond');
 console.log(result);            // cond, bond, sond, fond
 
-// squence - replacement text
+// sequence - replacement text
 
 result = text.replace(/(.at)/g, 'word ($1)');
 console.log(result);            // word (cat), word (bat), word (sat), word (fat)
+
+// htmlEscape()
+
+function htmlEscape(text) {
+  return text.replace(/[<>"&"]/g, function (match, pos, originalText) {
+    switch (match) {
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case "&":
+        return "&amp;";
+      case "\"":
+        return "&quot;";
+    }
+  });
+}
+
+console.log(htmlEscape('<p class="greeting">Hello world!</p>'));
