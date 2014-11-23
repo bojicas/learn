1b89c857c6fd517bcd076fbf4e86bfbb0c4c16b9
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-23 13:35:15 +0400

    The eval() method

--- javascript_pro/chapter_05/80_global_eval.js
+++ javascript_pro/chapter_05/80_global_eval.js
@@ -7,3 +7,6 @@ eval('console.log(msg)');		// hello world
 eval("function sayHi() { console.log('hi'); }");
 
 sayHi();	// hi
+
+eval("var message = 'hello world';");
+console.log(message);   // hello world
