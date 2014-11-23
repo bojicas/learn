de024f0931ae3de3494a1db92fa009ef1e7f42aa
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-23 13:56:34 +0400

    Math Object - min() and max() example apply() & arrays

--- javascript_pro/chapter_05/82_min_max.js
+++ javascript_pro/chapter_05/82_min_max.js
@@ -5,3 +5,12 @@ console.log(max); // 54
 
 var min = Math.min(3, 54, 32, 16);
 console.log(min); // 3
+
+// max/min in an array
+
+var values = [1, 2, 3, 4, 5, 6, 7, 8];
+var minInArray = Math.min.apply(Math, values);
+var maxInArray = Math.max.apply(Math, values);
+
+console.log(minInArray);  // 1
+console.log(maxInArray);  // 8
