e5c0ae11ddc76097f7b5260e9505aebd2ea2be84
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-23 19:58:58 +0400

    Math Object - the random() method

---
+++ javascript_pro/chapter_05/84_math_random.js
@@ -0,0 +1,21 @@
+// Math - The random() Method
+
+// formula:
+// Math.floor(Math.random() * total_number_of_choices + first_possible_value)
+
+// 1 to 10
+var num = Math.floor(Math.random() * 10 + 1);
+console.log(num);
+
+// 2 to 10
+num = Math.floor(Math.random() * 9 + 2);
+console.log(num);
+
+// use a function
+function selectFrom(lowerValue, upperValue) {
+  var choices = upperValue - lowerValue + 1;
+  return Math.floor(Math.random() * choices + lowerValue);
+}
+
+num = selectFrom(2, 10);
+console.log(num); // number between 2 and 10, inclusive
