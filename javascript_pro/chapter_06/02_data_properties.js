b3a1202033e31543b0b5c426f8bb5257d2a4d4e6
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-26 09:15:03 +0400

    Understanding Objects - data properties example (writable & configurable)

--- javascript_pro/chapter_06/02_data_properties.js
+++ javascript_pro/chapter_06/02_data_properties.js
@@ -2,10 +2,20 @@
 
 var person = {};
 Object.defineProperty(person, 'name', {
-  configurable: false,
+  writable: false,
   value: 'Nicholas'
 });
 
 console.log(person.name); // Nicholas
 person.name = 'Greg';
 console.log(person.name); // Nicholas
+
+var anotherPerson = {};
+Object.defineProperty(anotherPerson, 'name', {
+  configurable: false,
+  value: 'Nicholas'
+});
+
+console.log(anotherPerson.name);
+delete anotherPerson.name;
+console.log(anotherPerson.name);
