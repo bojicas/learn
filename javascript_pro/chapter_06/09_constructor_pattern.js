a46bb5b9ada19cf3cb716e78f377bbfc914ed7ef
Silviu D. Bojica <silviu@cubicleapps.com>
2014-11-29 21:18:41 +0400

    Object Creation - the constructor pattern

---
+++ javascript_pro/chapter_06/09_constructor_pattern.js
@@ -0,0 +1,18 @@
+// Object Creation - The Constructor Pattern
+
+function Person(name, age, job) {
+  this.name = name;
+  this.age = age;
+  this.job = job;
+  this.sayName = sayName;
+}
+
+function sayName() {
+  console.log(this.name);
+}
+
+var person1 = new Person('Nicholas', 29, 'Software Engineer');
+var person2 = new Person('Greg', 27, 'Doctor');
+
+person1.sayName();      // Nicholas
+person2.sayName();      // Greg
