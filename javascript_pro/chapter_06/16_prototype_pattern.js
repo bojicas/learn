21a3ed89818aa5eadb7884063e85da2bf33cce97
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-02 12:08:50 +0400

    Prototype Pattern - Alternate Prototype Syntax (including constructor property)

--- javascript_pro/chapter_06/16_prototype_pattern.js
+++ javascript_pro/chapter_06/16_prototype_pattern.js
@@ -3,6 +3,7 @@
 var Person = function () {};
 
 Person.prototype = {
+  constructor: Person,
   name: 'Nicholas',
   age: 29,
   job: 'Software Engineer',
@@ -13,3 +14,11 @@ Person.prototype = {
 
 var person = new Person();
 person.sayName(); // Nicholas
+
+var friend = new Person();
+console.log(friend instanceof Object);      // true
+console.log(friend instanceof Person);      // true
+console.log(friend.constructor == Person);  // true
+console.log(friend.constructor == Object);  // false
+
+// note: last 2 lines will have reversed results if constructor: Person is omitted
