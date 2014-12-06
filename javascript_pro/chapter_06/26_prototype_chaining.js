1bfd6a35f7d8a26bdb39ecbf7d2c1a72b150b186
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-06 18:04:51 +0000

    Prototype Chaining Example

---
+++ javascript_pro/chapter_06/26_prototype_chaining.js
@@ -0,0 +1,24 @@
+// Prototype Chaining Example
+
+function SuperType() {
+  this.property = true;
+}
+
+SuperType.prototype.getSuperValue = function () {
+  return this.property;
+};
+
+function SubType() {
+  this.subproperty = false;
+}
+
+// inherit from SuperType
+
+SubType.prototype = new SuperType();
+
+SubType.prototype.getSubValue = function () {
+  return this.subproperty;
+};
+
+var instance = new SubType();
+console.log(instance.getSuperValue());   //true
