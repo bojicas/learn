c58ad1275e08b5bce28357abff755d1bc254290a
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-07 18:53:06 +0000

    Prototype and instance relationships

--- javascript_pro/chapter_06/26_prototype_chaining.js
+++ javascript_pro/chapter_06/26_prototype_chaining.js
@@ -26,3 +26,7 @@ console.log(instance.getSuperValue());      // true
 console.log(instance instanceof Object);    // true
 console.log(instance instanceof SuperType); // true
 console.log(instance instanceof SubType);   // true
+
+console.log(Object.prototype.isPrototypeOf(instance));    // true
+console.log(SuperType.prototype.isPrototypeOf(instance)); // true
+console.log(SubType.prototype.isPrototypeOf(instance));   // true
