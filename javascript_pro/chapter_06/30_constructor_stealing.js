9eeba4aa85eedaf7aa83c1503a9bb2be3d46f1f5
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-08 18:29:10 +0000

    Constructor Stealing (object masquerading/classicl inheritance)

---
+++ javascript_pro/chapter_06/30_constructor_stealing.js
@@ -0,0 +1,17 @@
+// Constructor Stealing (object masquerading/classicl inheritance)
+
+function SuperType() {
+  this.colors = [ 'red', 'blue', 'green' ];
+}
+
+function SubType() {
+  // inherit from SuperType
+  SuperType.call(this);
+}
+
+var instance1 = new SubType();
+instance1.colors.push('black');
+console.log(instance1.colors);  // [ 'red', 'blue', 'green', 'black' ]
+
+var instance2 = new SubType();
+console.log(instance2.colors);  // [ 'red', 'blue', 'green' ]
