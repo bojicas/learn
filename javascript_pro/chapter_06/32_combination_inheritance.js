596088c5f3fae11d4e9664d67a78a9880ab0d1e9
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-09 12:11:30 +0400

    Combination Inheritance

--- javascript_pro/chapter_06/32_combination_inheritance.js
+++ javascript_pro/chapter_06/32_combination_inheritance.js
@@ -5,13 +5,13 @@ function SuperType(name) {
   this.colors = [ 'red', 'blue', 'green' ];
 }
 
-SuperType.prototype.sayName = function() {
+SuperType.prototype.sayName = function () {
   console.log(this.name);
 };
 
 function SubType(name, age) {
   // inherit properties
-  SuperType.call(this.name);
+  SuperType.call(this, name);
 
   this.age = age;
 }
@@ -21,15 +21,15 @@ SubType.prototype = new SuperType();
 
 SubType.prototype.sayAge = function () {
   console.log(this.age);
-}
+};
 
 var instance1 = new SubType('Nicholas', 29);
 instance1.colors.push('black');
 console.log(instance1.colors);  // [ 'red', 'blue', 'green', 'black' ]
-instance1.sayName();
-instance1.sayAge();
+instance1.sayName();            // Nicholas
+instance1.sayAge();             // 29
 
 var instance2 = new SubType('Greg', 27);
-console.log(instance2.colors);
-instance2.sayName();
-instance2.sayAge();
+console.log(instance2.colors);  // [ 'red', 'blue', 'green' ]
+instance2.sayName();            // Greg
+instance2.sayAge();             // 27
