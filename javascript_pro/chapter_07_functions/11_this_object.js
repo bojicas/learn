fea015c3ce058db60548f36cea784eecebe97ce4
Silviu D. Bojica <silviu@cubicleapps.com>
2014-12-15 09:40:34 +0400

    This Object Example

---
+++ javascript_pro/chapter_07_functions/11_this_object.js
@@ -0,0 +1,14 @@
+// This Object Example
+
+var name = 'The Window';
+
+var object = {
+  name: 'My Object',
+
+  getName: function () {
+    return this.name;
+  }
+};
+
+console.log(object.getName());    // My Object
+console.log((object.getName)());  // My Object
