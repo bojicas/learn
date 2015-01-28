3797d6693f97efa94a615f0198c43b99016543eb
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-28 11:00:59 +0000

    Declare Famous namespace variable

--- famous_meteor/meteor_famous_mobile/client/app.js
+++ famous_meteor/meteor_famous_mobile/client/app.js
@@ -10,3 +10,36 @@ App.helpers = {
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
 });
+
+_.extend(Famous, {
+  Engine: famous.core.Engine,
+  View: famous.core.View,
+  Surface: famous.core.Surface,
+  Modifier: famous.core.Modifier,
+  Transform: famous.core.Transform,
+  Draggable: famous.core.Draggable,
+  StateModifier: famous.modifiers.StateModifier,
+
+  RenderController: famous.views.RenderController,
+  EventHandler: famous.core.EventHandler,
+
+  ScrollView: famous.views.ScrollView,
+  HeaderFooterLayout: famous.views.HeaderFooterLayout,
+
+  Easing: famous.transitions.Easing,
+  Transitionable: famous.transitions.Transitionable,
+
+  GenericSync: famous.inputs.GenericSync,
+  MouseSync: famous.inputs.MouseSync,
+  TouchSync: famous.inputs.TouchSync,
+  Timer: famous.utilities.Timer,
+
+  FastClick: famous.inputs.Famous
+});
+
+Famous.GenericSync.register({
+  mouse: Famous.MouseSync,
+  touch: Famous.TouchSync
+});
+
+Logger.setLevel('famous-views', 'info');
