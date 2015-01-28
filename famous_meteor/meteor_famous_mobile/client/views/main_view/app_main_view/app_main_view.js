8ac5557a19a6a74ab455382eecf655857cad1bb1
Silviu D. Bojica <silviu@cubicleapps.com>
2015-01-28 11:04:44 +0000

    Add app main view as the entry screen

---
+++ famous_meteor/meteor_famous_mobile/client/views/main_view/app_main_view/app_main_view.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* AppMainView: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.AppMainView.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.AppMainView.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* AppMainView: Lifecycle Hooks */
+/*****************************************************************************/
+Template.AppMainView.created = function () {
+};
+
+Template.AppMainView.rendered = function () {
+};
+
+Template.AppMainView.destroyed = function () {
+};
