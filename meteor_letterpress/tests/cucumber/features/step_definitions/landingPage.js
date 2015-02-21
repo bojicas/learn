d9f32aa4d024a173e8157e04a0e959d8bb43e999
Silviu D. Bojica <silviu@cubicleapps.com>
2015-02-21 10:59:25 +0400

    Create a failing test

--- meteor_letterpress/tests/cucumber/features/step_definitions/landingPage.js
+++ meteor_letterpress/tests/cucumber/features/step_definitions/landingPage.js
@@ -1,13 +1,23 @@
+var assert = require('assert');
+
 module.exports = function () {
+  var helper = this;
+
   this.Given(/^I am a new visitor$/, function (callback) {
-    callback.pending();
+    callback();
   });
 
   this.When(/^I navigate to the landing page$/, function (callback) {
-    callback.pending();
+    helper.world.browser.
+      url(helper.world.mirrorUrl).
+      call(callback);
   });
 
   this.Then(/^I see the heading "([^"]*)"$/, function (arg1, callback) {
-    callback.pending();
+    helper.world.browser.
+      getText('h1', function (error, actualHeading) {
+        assert.equal(actualHeading, expectedHeading);
+        callback();
+      });
   });
 };
